1c9b7e4c803e8765b2b80e0c043f9383067c503b
DESKTOP-ML87TGU\Winfred Villaluna <winfred_villaluna@dlsu.edu.ph>
2017-11-19 09:22:53 +0800

    Adds handling of input after obtaining Inf cases

--- src/gui/CalculatorGUI.java
+++ src/gui/CalculatorGUI.java
@@ -142,106 +142,113 @@ public class CalculatorGUI implements MouseListener{
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		for(int i = 0; i < 20; i++) {
+		for(int i = 0; i < characterList.length; i++) {
 			if(e.getSource().equals(buttonList.get(i))) {
-				if(characterList[i].equals("\u232B")) {
-					System.out.println("HERE");
-					System.out.println(inputPanel.getText().length());
-					if(inputPanel.getText().length() != 1) {
-						inputPanel.setText(inputPanel.getText().substring(0, inputPanel.getText().length()-1));
-					}else {	
+				if(inputPanel.getText().contains("Infinity")) {
+					if(characterList[i].equals("CE")) {
 						inputPanel.setText("0");
-					}
-					
-				}else if(characterList[i].equals("CE")) {
-					inputPanel.setText("0");
-				}else if(characterList[i].equals("(")) {
-					char prevChar = inputPanel.getText().charAt(inputPanel.getText().length()-1);
-					if((prevChar == '+' || prevChar == '−' || prevChar == '*' || prevChar == '/' || prevChar == '%' || prevChar == '(')) {
-						inputPanel.setText(inputPanel.getText() + characterList[i]);
-						openPar++;
-					}else if(inputPanel.getText().length() == 1 && inputPanel.getText().charAt(0) == '0') {
-						inputPanel.setText(characterList[i]);
-						openPar++;
-					}
-					
-				}else if(characterList[i].equals(")")) {
-					char prevChar = inputPanel.getText().charAt(inputPanel.getText().length()-1);
-					if(openPar > 0) {
-						if(prevChar == '(' ) {
-							System.out.println(inputPanel.getText() + "0" + characterList[i]);
-							inputPanel.setText(inputPanel.getText() + "0" + characterList[i]);
-							openPar--;
-						}else if(!(prevChar == '+' || prevChar == '−' || prevChar == '*' || prevChar == '/' || prevChar == '%')) {
-							inputPanel.setText(inputPanel.getText() + characterList[i]);
-							openPar--;
-						}
-					}
-					
-				}else if(characterList[i].equals("+") || characterList[i].equals("−") || characterList[i].equals("*") || characterList[i].equals("/") || characterList[i].equals("%")){
-					char prevChar = inputPanel.getText().charAt(inputPanel.getText().length()-1);
-					if(prevChar == '+' || prevChar == '−' || prevChar == '*' || prevChar == '/' || prevChar == '%') {						
-						inputPanel.setText(inputPanel.getText().substring(0, inputPanel.getText().length()-1) + characterList[i]);
-					}else {
-						inputPanel.setText(inputPanel.getText() + characterList[i]);
-					}
-					
-				}else if(characterList[i].equals("=")) {
-					String input = inputPanel.getText().trim();
-					System.out.println("ENTERING THE TREE");
-					System.out.println("MY INPUT WILL BE: \"" + input + "\"");
-					inputPanel.setText(new CalculatorVisitor().solve(input.replaceAll("−", "-")).toString());
-					
-				}else if(characterList[i].equals(".")) {
-					if(inputPanel.getText().lastIndexOf(".") == -1) {
-						inputPanel.setText(inputPanel.getText() + characterList[i]);
-					}else {
-						boolean isPresent = false;
-						String toCheck = inputPanel.getText().trim();
-						for(int j = toCheck.length()-1; j > 0; j--) {
-							System.out.print(inputPanel.getText().charAt(j));
-							if(toCheck.charAt(j) == '+' 
-							|| toCheck.charAt(j) == '−'
-							|| toCheck.charAt(j) == '*'
-							|| toCheck.charAt(j) == '/'
-							|| toCheck.charAt(j) == '%') {
-									break;
-							}else if(toCheck.charAt(j) == '.') {
-								isPresent = true;
-							}
-						}
-						System.out.println();
-						System.out.println("ISPRESENT STATUS: " + isPresent);;
-						if(!isPresent) {
-							if(inputPanel.getText().charAt(inputPanel.getText().length()-1) >= '0' && inputPanel.getText().charAt(inputPanel.getText().length()-2) <= '9') {
-								inputPanel.setText(inputPanel.getText() + characterList[i]);
-							}else {
-								inputPanel.setText(inputPanel.getText() + "0" + characterList[i]);
-							}
-						}
-					}
-					
-				}else if(characterList[i].equals("-")){
-					System.out.println("FOUND A -");
-					char prevprevChar = inputPanel.getText().length() > 1 ? inputPanel.getText().charAt(inputPanel.getText().length()-2) : ' '; 
-					char prevChar = inputPanel.getText().charAt(inputPanel.getText().length()-1);
-					String prev = prevprevChar + "" +  prevChar;
-					
-					if((prevprevChar == '+' || prevprevChar == '−' || prevprevChar == '*' || prevprevChar == '/' || prevprevChar == '%' || prevprevChar == ' ') && prevChar == '0') {
-						inputPanel.setText(inputPanel.getText().substring(0, inputPanel.getText().length()-1) + characterList[i]);	
-					}else if(prevChar == '+' || prevChar == '−' || prevChar == '*' || prevChar == '/' || prevChar == '%' || prevChar == ' ' || prevChar == '(') {
-						inputPanel.setText(inputPanel.getText().substring(0, inputPanel.getText().length()) + characterList[i]);
-					}
-					
+					}	
 				}else {
-					char prevprevChar = inputPanel.getText().length() > 1 ? inputPanel.getText().charAt(inputPanel.getText().length()-2) : ' '; 
-					char prevChar = inputPanel.getText().charAt(inputPanel.getText().length()-1);
-					String prev = prevprevChar + "" +  prevChar;
-					
-					if((prevprevChar == '+' || prevprevChar == '−' || prevprevChar == '*' || prevprevChar == '/' || prevprevChar == '%' || prevprevChar == ' ') && prevChar == '0') {
-						inputPanel.setText(inputPanel.getText().substring(0, inputPanel.getText().length()-1) + characterList[i]);	
+					if(characterList[i].equals("CE")) {
+						inputPanel.setText("0");
+					}else if(characterList[i].equals("\u232B")) {
+						System.out.println("HERE");
+						System.out.println(inputPanel.getText().length());
+						if(inputPanel.getText().length() != 1) {
+							inputPanel.setText(inputPanel.getText().substring(0, inputPanel.getText().length()-1));
+						}else {	
+							inputPanel.setText("0");
+						}
+						
+					}else if(characterList[i].equals("(")) {
+						char prevChar = inputPanel.getText().charAt(inputPanel.getText().length()-1);
+						if((prevChar == '+' || prevChar == '−' || prevChar == '*' || prevChar == '/' || prevChar == '%' || prevChar == '(')) {
+							inputPanel.setText(inputPanel.getText() + characterList[i]);
+							openPar++;
+						}else if(inputPanel.getText().length() == 1 && inputPanel.getText().charAt(0) == '0') {
+							inputPanel.setText(characterList[i]);
+							openPar++;
+						}
+						
+					}else if(characterList[i].equals(")")) {
+						char prevChar = inputPanel.getText().charAt(inputPanel.getText().length()-1);
+						if(openPar > 0) {
+							if(prevChar == '(' ) {
+								System.out.println(inputPanel.getText() + "0" + characterList[i]);
+								inputPanel.setText(inputPanel.getText() + "0" + characterList[i]);
+								openPar--;
+							}else if(!(prevChar == '+' || prevChar == '−' || prevChar == '*' || prevChar == '/' || prevChar == '%')) {
+								inputPanel.setText(inputPanel.getText() + characterList[i]);
+								openPar--;
+							}
+						}
+						
+					}else if(characterList[i].equals("+") || characterList[i].equals("−") || characterList[i].equals("*") || characterList[i].equals("/") || characterList[i].equals("%")){
+						char prevChar = inputPanel.getText().charAt(inputPanel.getText().length()-1);
+						if(prevChar == '+' || prevChar == '−' || prevChar == '*' || prevChar == '/' || prevChar == '%') {						
+							inputPanel.setText(inputPanel.getText().substring(0, inputPanel.getText().length()-1) + characterList[i]);
+						}else {
+							inputPanel.setText(inputPanel.getText() + characterList[i]);
+						}
+						
+					}else if(characterList[i].equals("=")) {
+						String input = inputPanel.getText().trim();
+						System.out.println("ENTERING THE TREE");
+						System.out.println("MY INPUT WILL BE: \"" + input + "\"");
+						inputPanel.setText(new CalculatorVisitor().solve(input.replaceAll("−", "-")).toString());
+						
+					}else if(characterList[i].equals(".")) {
+						if(inputPanel.getText().lastIndexOf(".") == -1) {
+							inputPanel.setText(inputPanel.getText() + characterList[i]);
+						}else {
+							boolean isPresent = false;
+							String toCheck = inputPanel.getText().trim();
+							for(int j = toCheck.length()-1; j > 0; j--) {
+								System.out.print(inputPanel.getText().charAt(j));
+								if(toCheck.charAt(j) == '+' 
+								|| toCheck.charAt(j) == '−'
+								|| toCheck.charAt(j) == '*'
+								|| toCheck.charAt(j) == '/'
+								|| toCheck.charAt(j) == '%') {
+										break;
+								}else if(toCheck.charAt(j) == '.') {
+									isPresent = true;
+								}
+							}
+							System.out.println();
+							System.out.println("ISPRESENT STATUS: " + isPresent);;
+							if(!isPresent) {
+								if(inputPanel.getText().charAt(inputPanel.getText().length()-1) >= '0' && inputPanel.getText().charAt(inputPanel.getText().length()-2) <= '9') {
+									inputPanel.setText(inputPanel.getText() + characterList[i]);
+								}else {
+									inputPanel.setText(inputPanel.getText() + "0" + characterList[i]);
+								}
+							}
+						}
+						
+					}else if(characterList[i].equals("-")){
+						System.out.println("FOUND A -");
+						char prevprevChar = inputPanel.getText().length() > 1 ? inputPanel.getText().charAt(inputPanel.getText().length()-2) : ' '; 
+						char prevChar = inputPanel.getText().charAt(inputPanel.getText().length()-1);
+						String prev = prevprevChar + "" +  prevChar;
+						
+						if((prevprevChar == '+' || prevprevChar == '−' || prevprevChar == '*' || prevprevChar == '/' || prevprevChar == '%' || prevprevChar == ' ') && prevChar == '0') {
+							inputPanel.setText(inputPanel.getText().substring(0, inputPanel.getText().length()-1) + characterList[i]);	
+						}else if(prevChar == '+' || prevChar == '−' || prevChar == '*' || prevChar == '/' || prevChar == '%' || prevChar == ' ' || prevChar == '(') {
+							inputPanel.setText(inputPanel.getText().substring(0, inputPanel.getText().length()) + characterList[i]);
+						}
+						
 					}else {
-						inputPanel.setText(inputPanel.getText() + characterList[i]);
+						System.out.println("A NUMBER");
+						char prevprevChar = inputPanel.getText().length() > 1 ? inputPanel.getText().charAt(inputPanel.getText().length()-2) : ' '; 
+						char prevChar = inputPanel.getText().charAt(inputPanel.getText().length()-1);
+						String prev = prevprevChar + "" +  prevChar;
+						
+						if((prevprevChar == '+' || prevprevChar == '−' || prevprevChar == '*' || prevprevChar == '/' || prevprevChar == '%' || prevprevChar == ' ') && prevChar == '0') {
+							inputPanel.setText(inputPanel.getText().substring(0, inputPanel.getText().length()-1) + characterList[i]);	
+						}else {
+							inputPanel.setText(inputPanel.getText() + characterList[i]);
+						}
 					}
 				}
 			}
